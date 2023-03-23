f3694d43aed1230488383037e69ada4227e51adb
“GigGit” <front@fedhubs.fr>
2023-03-23 17:46:13 +0100

    23/03/2023

--- index.js
+++ index.js
@@ -1,70 +1,52 @@
-let play = {
-    name: "Giovanni",
-    chips: 120
+let myLeads = `[]`
+myLeads = JSON.parse(myLeads)
+const inputEl = document.querySelector("#input-el")
+const btn = document.querySelector('#input-btn')
+const deleteBtn = document.querySelector('#delete-btn')
+const tabBtn = document.querySelector('#tab-btn')
+const ulEl = document.querySelector("#ul-el")
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if(leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
 }
 
-let cards = []
-let sum = 0
-let hasBlackJack = false
-let isAlive = false
-let message = ""
-let button = document.querySelector('#button')
-let messageEl = document.querySelector('#message')
-let result = document.querySelector('#sumEl')
-let card = document.querySelector('#cards')
-let newCard = document.querySelector('#card-button')
-let player = document.querySelector('#player')
-
-player.textContent = play.name + ': $' + play.chips
-
-button.addEventListener('click', startGame)
-
-newCard.addEventListener('click', newCards)
-
-function getRandomCard() {
-    let random = Math.floor(Math.random() * 12) + 1
-    console.log(random)
-    if(random === 1) {
-        return 11
-    } else if(random > 10) {
-        return 10
-    } else {
-        return random 
+function render(leads) {
+    let listItems = ""
+    for(let i = 0 ; i < leads.length ; i++) {
+        listItems += `
+        <li>
+            <a target='_blank' href='${leads[i]}'>
+                ${leads[i]}
+            </a>
+        </li>
+        `
     }
+    ulEl.innerHTML = listItems
 }
 
-function startGame() {
-    let random1 = getRandomCard()
-    let random2 = getRandomCard()
-    cards.push(random1, random2)
-    sum = random1 + random2
-    isAlive = true
-    renderGame()
-}
+tabBtn.addEventListener('click', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
 
-function newCards() {
-    if(isAlive && hasBlackJack === false) {
-        let card_play = getRandomCard()
-        sum += card_play
-        cards.push(card_play) 
-        renderGame()
-    }
-}
+deleteBtn.addEventListener('click', function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+    console.log(localStorage.getItem("myLeads"))
+})
 
-function renderGame() {
-    if(sum <= 20) {
-        message = "Do you want to draw a new card ?"
-    } else if(sum === 21) {
-        message = "Wohoo ! You've got Blackjack !"
-        hasBlackJack = true
-    } else {
-        message = "You're out of the game !"
-        isAlive = false
-    }
-    messageEl.textContent = message
-    result.textContent = 'Sum : ' + sum
-    card.textContent = 'Cards: '
-    for(let i = 0 ; i < cards.length ; i++) {
-        card.textContent = 'Cards: ' + cards.join(' ')
-    }
-}
+btn.addEventListener('click', function(){
+    myLeads.push(inputEl.value)
+    inputEl.value = ''
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+    console.log(localStorage.getItem("myLeads"))
+})
+
